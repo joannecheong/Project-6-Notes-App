8dea404909cbce00b0d2a22554bb6123ab5ea9c8
Joanne <joanne.ye@uconn.edu>
2020-03-04 13:55:43 -0500

    Implete the delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,21 +7,34 @@ import { useState } from "react";
 
 function App() {
   const [notes, setNotes] = useState([]);
+
   function addNote(note) {
     setNotes(prevNotes => {
-      return[
-        ...prevNotes, note
-      ]
-    })
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
   }
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-     { notes.map((noteItem, index) => {
+      {notes.map((noteItem, index) => {
         return (
-          <Note key={index} id={index} title={noteItem.title} content={noteItem.content} />
-        )
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
       })}
       <Footer />
     </div>
